88cd3fef841f63053b042ecd7e8bd552ccd3bf74
Bamboofungus <anthony.chang1010@gmail.com>
2024-05-19 21:25:55 +0900

    replaced replay buffer

--- lunarlander.py
+++ lunarlander.py
@@ -1,5 +1,4 @@
 import gymnasium as gym
-import tensorboard
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -7,11 +6,17 @@ from torch.optim import Adam
 from torch.distributions import Categorical
 import numpy as np
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Any
 from collections import namedtuple
+from torch.types import Number
+from tqdm import tqdm
 
-total_rewards = []
-time_per_episode = []
+import wandb
+
+if not torch.cuda.is_available():
+    print("gpu not detected, falling back to cpu")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def manual_policy(obs) -> int:
     """
@@ -33,25 +38,56 @@ def manual_policy(obs) -> int:
     # default
     return 0
 
-ReplayEntry = namedtuple('ReplayEntry', ['observation', 'action', 'reward', 'next_observation', 'value', 'log_prob', 'rtg', 'advantage'])
+BufferEntry = namedtuple('BufferEntry', ['observation', 'action', 'reward', 'value', 'log_prob', 'rtg', 'advantage'])
 
-class ReplayBuffer():
+class TrajectoryBuffer():
     """
-    TODO
+    Buffer for storing data from policy interacting with environment for use in training at end
     """
-    def __init__(self, batch_size, buffer_size=10000):
-        self._buffer = []
-        self.batch_size = batch_size
+    def __init__(self, buffer_size, gamma):
+        self.buffer = []
         self.buffer_size = buffer_size
+        self.curr_trajectory_start = 0
+        self.gamma = gamma
 
-    def append(self, entry):
-        self._buffer.append(entry)
-        # popleft if buffer > max size
-        if(len(self._buffer) > self.buffer_size):
-            self._buffer = self._buffer[1:]
+    def end_trajectory(self, trajectory_end, final_value=0):
+        curr_rtg = final_value
 
-    def sample(self):
-        return np.random.choice(self._buffer, self.batch_size, replace=False)
+        for t in reversed(range(self.curr_trajectory_start, trajectory_end + 1)):
+            curr_rtg = self.buffer[t].reward + curr_rtg * self.gamma
+            self.buffer[t] = BufferEntry(
+                observation=self.buffer[t].observation,
+                action=self.buffer[t].action,
+                reward=self.buffer[t].reward,
+                value=self.buffer[t].value,
+                log_prob=self.buffer[t].log_prob,
+                rtg=curr_rtg,
+                advantage=curr_rtg - self.buffer[t].value
+            )
+
+        self.curr_trajectory_start = trajectory_end + 1
+
+
+    def get_trajectories(self):
+        # assert len(self._buffer) > self.batch_size, "TrajectoryBuffer not populated enough to sample from"
+        # indices = np.random.choice(len(self._buffer), self.batch_size, replace=False)
+        # batch = [self._buffer[idx] for idx in indices]
+
+        # separate observations, actions, etc. into different tensors
+        observations, actions, rewards, values, log_probs, rtgs, advantages = zip(*self.buffer)
+        observations = torch.tensor(np.array(observations), dtype=torch.float32).to(device)
+        actions = torch.tensor(np.array(actions), dtype=torch.float32).to(device)
+        rewards = torch.tensor(np.array(rewards), dtype=torch.float32).to(device)
+        values = torch.tensor(np.array(values), dtype=torch.float32).to(device)
+        log_probs = torch.tensor(np.array(log_probs), dtype=torch.float32).to(device)
+        rtgs = torch.tensor(np.array(rtgs), dtype=torch.float32).to(device)
+        advantages = torch.tensor(np.array(advantages), dtype=torch.float32).to(device)
+
+        self.buffer = []
+        self.curr_trajectory_start = 0
+
+        return observations, actions, rewards, values, log_probs, rtgs, advantages
+
 
 class PolicyNN(nn.Module):
     """
@@ -60,7 +96,7 @@ class PolicyNN(nn.Module):
     Outputs a probability for each action from the passed state which can then be sampled
     to choose the next action as well as log_prob for calculating gradients
     """
-    def __init__(self, input_size=8, hidden_size=128, output_size=4, lr=1e-4):
+    def __init__(self, input_size=8, hidden_size=128, output_size=4, lr=1e-4, epsilon=0.05):
         super(PolicyNN, self).__init__()
         self.network = nn.Sequential(
             nn.Linear(input_size, hidden_size),
@@ -68,26 +104,30 @@ class PolicyNN(nn.Module):
             nn.Linear(hidden_size, output_size),
             nn.Softmax(dim=-1) # Normalize probabilties to sum to one
         )
-        self.epsilon = 0.05
+        self.epsilon = epsilon
         self.optimizer = Adam(self.parameters(), lr)
 
     def forward(self, x):
-        x = torch.tensor(np.array(x))
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x).to(device)
         action_probs = self.network(x).squeeze()
         return Categorical(action_probs)
 
-    def act(self, observation):
-        action_dist = self.forward(observation)
-        if np.random.rand() < self.epsilon:
-            action = torch.tensor(np.random.randint(0, 3))
-        else:
-            action = action_dist.sample()
-        return action.item(), action_dist.log_prob(action)
+    def act(self, observation) -> Tuple[Number, float]:
+        with torch.no_grad():
+            action_dist = self.forward(observation)
+            if np.random.rand() < self.epsilon:
+                action = torch.tensor(np.random.randint(0, 3)).to(device)
+            else:
+                action = action_dist.sample()
+            log_prob = action_dist.log_prob(action).item()
+        return action.item(), log_prob
 
-    def loss(self, observation, action, advantage, log_prob):
-        action_dist = self.forward(observation, action)
-        log_prob = action_dist.log_prob(action)
-        return (log_prob * advantage)
+    def loss(self, observations, actions, advantages, old_log_probs):
+        action_dists = self.forward(observations)
+        log_probs = action_dists.log_prob(actions)
+        # important: negate this loss since it's meant for gradient ascent
+        return - (log_probs * advantages).mean()
 
 class ValueNN(nn.Module):
     """
@@ -102,93 +142,144 @@ class ValueNN(nn.Module):
         )
         self.optimizer = Adam(self.parameters(), lr)
 
-    def forward(self, x) -> int:
-        # TODO support batching
-        x = torch.tensor(np.array(x))
+    def forward(self, x) -> float:
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x).to(device)
         return self.network(x).item()
 
-    def loss(self, observation, rtg):
-        return (self.forward(observation) - rtg)**2
+    def loss(self, observations: torch.Tensor, rtgs: torch.Tensor):
+        # call network directly here since forward doesn't support batching
+        return ((self.network(observations) - rtgs)**2).mean()
 
 
 def train():
-    env = gym.make("LunarLander-v2", render_mode="human")
+    env = gym.make("LunarLander-v2", render_mode=None)
     observation, _ = env.reset(seed=42)
 
-    EPOCHS = 1
-    EPISODES_PER_EPOCH = 10
-    BATCH_SIZE = 32
+    EPOCHS = 50
+    EPISODE_STEPS_PER_EPOCH = 4000
+    # BATCH_SIZE = 256
     GAMMA = 0.99
-    BUFFER_SIZE = 5000
+    POLICY_LEARNING_RATE = 1e-3
+    VALUE_LEARNING_RATE = 3e-3
+    EPSILON = 0.05
+    LOGGING = False
+    VALUE_STEPS_PER_EPOCH = 10
+    
+    wandb.login()
+    run = wandb.init(
+        # Set the project where this run will be logged
+        project="lunarlander_vpg",
+        # Track hyperparameters and run metadata 
+        mode= "online" if LOGGING else "disabled", 
+        config={ 
+            "epochs": EPOCHS,
+            # "batch_size": BATCH_SIZE,
+            "policy_learning_rate": POLICY_LEARNING_RATE,
+            "value_learning_rate": VALUE_LEARNING_RATE,
+            "epsilon": EPSILON,
+        },
+    )
 
-    policy = PolicyNN()
+    policy = PolicyNN(lr=POLICY_LEARNING_RATE, epsilon=EPSILON).to(device)
     # function approximation must be used here since LunarLander's observation_space is continous
-    f_approximator = ValueNN()
-    replay_buffer = ReplayBuffer(BATCH_SIZE, BUFFER_SIZE)
-
-    env = gym.make("LunarLander-v2", render_mode=None)
-    TrajectoryStep = namedtuple('TrajectoryStep', ['observation', 'action', 'reward', 'next_observation', 'value', 'log_prob'])
-    for _ in range(EPOCHS):
-        times_per_episode = []
-        total_rewards = []
-        for episode in range(EPISODES_PER_EPOCH):
-            observation, info = env.reset(seed=42)
-
-            # initalize per episode variables
-            time = 0
-            total_reward = 0
-            terminated = truncated = False
-
-            # save to list first so that rewards-to-go and advantages can be calculated before appending to ReplayBuffer
-            trajectory = []
-            while not terminated or truncated:
-                action, log_prob = policy.act(observation)
-                value = f_approximator(observation)
-                next_observation, reward, terminated, truncated, _ = env.step(action)
-                total_reward += reward
-                trajectory.append(TrajectoryStep(observation=observation, action=action, reward=reward, next_observation=next_observation, value=value, log_prob=log_prob))
-
-                observation = next_observation
-                time += 1
-
-            T = len(trajectory)
-            # TODO
-            if truncated:
-                curr_rtg = f_approximator(observation)
-            else:
-                curr_rtg = 0
-
-            # calculate rewards-to-go
-            rewards_to_go = [0] * T
-            for t in reversed(range(T)):
-                curr_rtg = trajectory[t].reward + curr_rtg * GAMMA
-                rewards_to_go[t] = curr_rtg
-            
-            # calculate advantages
-            advantages = [(rtg - trajectory[t].value) for (t, rtg) in enumerate(rewards_to_go)]
-            for t in range(T):
-                replay_buffer.append(ReplayEntry(
-                    observation=trajectory[t].observation,
-                    action=trajectory[t].action,
-                    reward=trajectory[t].reward,
-                    next_observation=trajectory[t].next_observation,
-                    value=trajectory[t].value,
-                    log_prob=trajectory[t].log_prob,
-                    rtg=rewards_to_go[t],
-                    advantage=advantages[t]
-                ))
+    f_approximator = ValueNN(lr=VALUE_LEARNING_RATE).to(device)
+    replay_buffer = TrajectoryBuffer(EPISODE_STEPS_PER_EPOCH, GAMMA)
 
 
-            total_rewards.append(total_reward)
-            times_per_episode.append(time)
+    # initialize per episode variables
+    rewards_per_episode = []
+    values_per_episode = []
+    episode_length = 0
+    total_reward = 0
+    terminated = truncated = False
 
-        # optimize for one epoch
+    for epoch in range(EPOCHS):
+        print(f"Running epoch #{epoch}")
+
+        for episode_step in tqdm(range(EPISODE_STEPS_PER_EPOCH)):
+            # initalize per epoch variables
+            terminated_episodes = 0
+            action, log_prob = policy.act(observation)
+            value = f_approximator(observation)
+            next_observation, reward, terminated, truncated, _ = env.step(action)
+            total_reward += reward
+            rewards_per_episode.append(reward)
+            values_per_episode.append(value)
+
+            # rtg and advantage will be calculated at trajectory end
+            replay_buffer.buffer.append(
+                BufferEntry(observation=observation, action=action, reward=reward, value=value, log_prob=log_prob, rtg=np.nan, advantage=np.nan)
+            )
+
+            observation = next_observation
+            episode_length += 1
+
+            # ensure bootstrapping correct for when time limit vs. terminal state reached (https://gymnasium.farama.org/environments/box2d/lunar_lander/#episode-termination)
+            epoch_ended = episode_step == (EPISODE_STEPS_PER_EPOCH - 1)
+            if truncated or terminated or epoch_ended:
+                if truncated or (episode_step == epoch_ended):
+                    final_value = f_approximator(observation)
+                else:
+                    final_value = 0
+                    terminated_episodes += 1
+                replay_buffer.end_trajectory(episode_step, final_value)
+                # carry over episodes between epochs
+                if not epoch_ended and not (epoch == EPOCHS - 1):
+                    observation, info = env.reset(seed=42)
+
+                    # log end of episode info
+                    wandb.log({
+                        "episode_length": episode_length,
+                        "advantages": wandb.Histogram([entry.advantage for entry in replay_buffer.buffer[episode_step:episode_step + episode_length]]),
+                        "rewards": wandb.Histogram(rewards_per_episode),
+                        "values": wandb.Histogram(values_per_episode),
+                        "reward_per_episode": total_reward
+                    })
+
+                    # reset per episode variables
+                    rewards_per_episode = []
+                    values_per_episode = []
+                    episode_length = 0
+                    total_reward = 0
+                    terminated = truncated = False
+
+        observations, actions, rewards, values, log_probs, rtgs, advantages = replay_buffer.get_trajectories()
+        old_policy_params = torch.cat([param.view(-1) for param in policy.parameters()])
+        old_value_params = torch.cat([param.view(-1) for param in f_approximator.parameters()])
+
+        # optimize for one epoch TODO use experiences more efficiently
+        policy.optimizer.zero_grad()
+        policy_loss = policy.loss(observations, actions, advantages, log_probs)
+        policy_loss.backward()
+        policy.optimizer.step()
+
+        for _ in range(VALUE_STEPS_PER_EPOCH):
+            f_approximator.optimizer.zero_grad()
+            value_loss = f_approximator.loss(observations, rtgs)
+            value_loss.backward()
+            f_approximator.optimizer.step()
         
+        # per epoch metrics
+        new_policy_params = torch.cat([param.view(-1) for param in policy.parameters()])
+        new_value_params = torch.cat([param.view(-1) for param in f_approximator.parameters()])
+        policy_step_MSE = ((new_policy_params - old_policy_params) ** 2).mean().item()
+        value_step_MSE = ((new_value_params - old_value_params) ** 2).mean().item()
+
+        policy_gradient_norms = [p.grad.norm().item() for p in policy.parameters()]
+        value_gradient_norms = [p.grad.norm().item() for p in f_approximator.parameters()]
+
+        wandb.log({
+            "epoch": epoch,
+            "policy_loss": policy_loss,
+            "value_loss": value_loss,
+            "policy_gradient_norms": max(policy_gradient_norms), # used for checking for gradient exploding/vanishing
+            "value_gradient_norms": max(value_gradient_norms),
+            "policy_step_MSE": policy_step_MSE, # used for lr tuning
+            "value_step_MSE": value_step_MSE
+        })
     env.close()
     return policy
-
-def optimize(policy, f_approximator, _buffer):
-
 
 if __name__ == "__main__":
     train()
